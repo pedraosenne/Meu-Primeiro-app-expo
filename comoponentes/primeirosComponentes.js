ffd666b1be43ccb115fbeada7f600dcd766e8ab7
pedro_senne <pedro.senne.santos@escola.pr.gov.br>
2025-09-23 11:28:55 +0000

    gfdxzh

--- comoponentes/primeirosComponentes.js
+++ comoponentes/primeirosComponentes.js
@@ -1,5 +1,14 @@
 import react from 'react'
 
  export default function Meunome(){
-    <h1> Pedro Henrique de Senne dos Santos</h1>
+    return(
+      <>
+      <h1>Pedro</h1>
+      </>
+
+               
+    )
+    
+    
+    
 }
